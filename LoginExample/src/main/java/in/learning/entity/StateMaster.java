3cb1d240f230c780149685f8f568f56ce8de4c1b
surya2dot0 <suryap1802@gmail.com>
2023-05-02 12:35:10 +0530

    Added state entity.

--- LoginExample/src/main/java/in/learning/entity/StateMaster.java
+++ LoginExample/src/main/java/in/learning/entity/StateMaster.java
@@ -16,5 +16,23 @@ public class StateMaster {
 	
 	private Integer countryId;
 	private String stateName;
+	public Integer getStateId() {
+		return stateId;
+	}
+	public void setStateId(Integer stateId) {
+		this.stateId = stateId;
+	}
+	public Integer getCountryId() {
+		return countryId;
+	}
+	public void setCountryId(Integer countryId) {
+		this.countryId = countryId;
+	}
+	public String getStateName() {
+		return stateName;
+	}
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
 	
 }
