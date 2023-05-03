133edb40b7f28ff75caba73dccfeaaf97dd42522
surya2dot0 <suryap1802@gmail.com>
2023-05-03 23:31:35 +0530

    All the form data

---
+++ LoginExample/src/main/java/in/learning/binding/UnLockAccForm.java
@@ -0,0 +1,35 @@
+package in.learning.binding;
+
+public class UnLockAccForm {
+	
+ private String email;
+ private String tempPwd;
+ private String newPwd;
+ private String confirmPwd;
+public String getTempPwd() {
+	return tempPwd;
+}
+public void setTempPwd(String tempPwd) {
+	this.tempPwd = tempPwd;
+}
+public String getNewPwd() {
+	return newPwd;
+}
+public void setNewPwd(String newPwd) {
+	this.newPwd = newPwd;
+}
+public String getConfirmPwd() {
+	return confirmPwd;
+}
+public void setConfirmPwd(String confirmPwd) {
+	this.confirmPwd = confirmPwd;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+ 
+ 
+}
---
+++ LoginExample/src/main/java/in/learning/binding/UserForm.java
@@ -0,0 +1,75 @@
+package in.learning.binding;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class UserForm {
+
+	private String fname;
+	private String lname;
+	private String email;
+	private Long phno;
+	private LocalDate dob;
+	private String gender;
+	private Integer countryId;
+	private Integer stateId;
+	private Integer cityId;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getPhno() {
+		return phno;
+	}
+	public void setPhno(Long phno) {
+		this.phno = phno;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Integer getCountryId() {
+		return countryId;
+	}
+	public void setCountryId(Integer countryId) {
+		this.countryId = countryId;
+	}
+	public Integer getStateId() {
+		return stateId;
+	}
+	public void setStateId(Integer stateId) {
+		this.stateId = stateId;
+	}
+	public Integer getCityId() {
+		return cityId;
+	}
+	public void setCityId(Integer cityId) {
+		this.cityId = cityId;
+	}
+	
+	
+}
